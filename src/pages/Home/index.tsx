b86e1e540be7fd8bb32ec2d63117752b3ec14232
rizidorio <rferreira013@hotmail.com>
2020-08-05 15:55:51 -0300

    Novos Logos e Layout Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiHelpCircle, FiSearch } from 'react-icons/fi';
 
-import logo from '../../assets/logo1.png';
-import fundo from '../../assets/fundo1.png';
+import logo from '../../assets/logo_emanut_200.png';
+import fundo from '../../assets/fundo_home.png';
 
 import './styles.css';
-import { Link } from 'react-router-dom';
+
 
 const Home = () => {
     return (
@@ -12,16 +14,22 @@ const Home = () => {
             <div id="page-home-content" className="container">
                 <div className="logo-container">
                     <img src={logo} alt="Logotipo" />
-                    <h2>Seu site de pesquisa de profissionais especializados em manutenções diversas.</h2>
+                    <h2>Seu site de pesquisa de profissionais especializados em manutenções diversas</h2>
                 </div>
                 <img src={fundo} alt="fundo" className="back-img"/>
 
                 <div className="botoes-container">
                     <Link to="/saber-mais">
-                        Sou um profissional e quero saber mais.
+                        <span>
+                            <FiHelpCircle />
+                        </span>
+                        Sou um profissional.
                     </Link>
                     <Link to="/categorias">
-                        Quero encontrar um profissional.
+                        <span>
+                            <FiSearch />
+                        </span>
+                        Procuro um profissional.
                     </Link>
                 </div>
             </div> 
