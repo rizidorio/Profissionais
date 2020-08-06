3b63da2b49cea4dea95ffd76daa29f336b1a4bef
rizidorio <rferreira013@hotmail.com>
2020-08-06 15:57:22 -0300

    Alterações home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiHelpCircle, FiSearch } from 'react-icons/fi';
+import { FiCheckSquare, FiSearch } from 'react-icons/fi';
 
-import logo from '../../assets/logo_emanut_200.png';
+import logo from '../../assets/logo_1200.png';
 import fundo from '../../assets/fundo_home.png';
 
 import './styles.css';
@@ -14,22 +14,25 @@ const Home = () => {
             <div id="page-home-content" className="container">
                 <div className="logo-container">
                     <img src={logo} alt="Logotipo" />
+                    <Link to="/login" className="login">
+                        Login
+                    </Link>
                     <h2>Seu site de pesquisa de profissionais especializados em manutenções diversas</h2>
                 </div>
-                <img src={fundo} alt="fundo" className="back-img"/>
+                <img src={fundo} alt="Imagem background" className="back-img"/>
 
                 <div className="botoes-container">
                     <Link to="/saber-mais">
                         <span>
-                            <FiHelpCircle />
+                            <FiCheckSquare />
                         </span>
-                        Sou um profissional.
+                        Sou um profissional
                     </Link>
                     <Link to="/categorias">
                         <span>
                             <FiSearch />
                         </span>
-                        Procuro um profissional.
+                        Procuro um profissional
                     </Link>
                 </div>
             </div> 
