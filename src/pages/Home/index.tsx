cf08a61a60872a50cd9ad7b8c5cd533353d33a8e
Ricardo izidorio Ferreira <42355541+rizidorio@users.noreply.github.com>
2020-08-17 10:14:06 -0300

    Alteração logo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiCheckSquare, FiSearch } from 'react-icons/fi';
 
-import logo from '../../assets/logo_1200.png';
+import logo from '../../assets/logo_profissionais.png';
 import fundo from '../../assets/fundo_home.png';
 
 import './styles.css';
@@ -14,7 +14,7 @@ const Home = () => {
             <div id="page-home-content" className="container">
                 <div className="logo-container">
                     <img src={logo} alt="Logotipo" />
-                    <h2>Seu site de pesquisa de profissionais especializados em manutenções diversas.</h2>
+                    <h2>Seu site de pesquisa de profissionais especializados nas mais diversas atividades.</h2>
                 </div>
                 <img src={fundo} alt="Imagem background" className="back-img"/>
 
